f1faaf51712a1613c04659e8dcb6401d93fdb58e
Tiago de lima <tiago_lima411@hotmail.com>
2018-01-25 22:17:26 -0300

    testes dao finalizado

--- src/test/java/testesMetodosDao/TesteUsuarioDao.java
+++ src/test/java/testesMetodosDao/TesteUsuarioDao.java
@@ -5,12 +5,20 @@
  */
 package testesMetodosDao;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import static com.mongodb.client.model.Filters.eq;
 import dao.IDao;
 import dao.UsuarioDao;
 import java.util.ArrayList;
 import java.util.List;
 import junit.framework.TestCase;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 import org.junit.Test;
+import util.MongoConnection;
 import vo.Endereco;
 import vo.Usuario;
 
@@ -19,111 +27,153 @@ import vo.Usuario;
  * @author tiago
  */
 public class TesteUsuarioDao extends TestCase{
+    UsuarioDao usuarioDAO;
+    Usuario usuario1;
+    Usuario usuario2;
+    MongoCollection<Usuario> collection; 
     
-    @Test
-    public void testDeveriaInserirUsuarioNoBanco(){
-        
-        Usuario usuario = new Usuario(
-            "Usuario Teste",
-            "000",
-            "123456",
-            "email@hotmail.com",   
-            "12341234",
-            "43214312",
-            false,
-            new Endereco(
-                "Maceio",
-                "B Bentes",
-                "57084-040",
-                "A 40",
-                "44",
-                ""
-            )
+    @Override
+    protected void setUp() throws Exception {
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+        collection = MongoConnection.getInstance().getDB()
+                .getCollection("Usuarios", Usuario.class)
+                .withCodecRegistry(pojoCodecRegistry);
+
+        usuario1 = new Usuario(
+                "Usuario Teste",
+                "000",
+                "123456",
+                "email@hotmail.com",
+                "12341234",
+                "43214312",
+                false,
+                new Endereco(
+                        "Maceio",
+                        "B Bentes",
+                        "57084-040",
+                        "A 40",
+                        "44",
+                        ""
+                )
         );
         
-        UsuarioDao usuarioDao = new UsuarioDao();
-        usuarioDao.salvar(usuario);        
-        usuarioDao.removerPorAtributo("nomeCompleto", "Tiago de Lima Alves");
-    }
-    /*
-    @Test
-    public void deveSalvarUmaListaDeUsuarios(){
-        List<Usuario> usuario = new ArrayList<Usuario>();
-        for(int i=0; i<100; i++){
-            usuario.add(new Usuario("Tiago de Lima Alves","045","123455","hotmail.com","uuuu9999","88888888",new Endereco("Maceio","Tabuleirio","57084040","A 40","007","")));
-        }
-        UsuarioDao usuarioDao = new UsuarioDao();
-        usuarioDao.salvarLista(usuario);
-        usuarioDao.removerPorAtributo("nomeCompleto", "Tiago de Lima Alves");
-    }
-         
-    @Test
-    public void deveListarTodosOsUsuarios(){
-        List<Usuario> usuarioInserir = new ArrayList<Usuario>();
-        for(int i=0; i<100; i++){
-            usuarioInserir.add(new Usuario("Tiago de Lima Alves","000","123455","hotmail.com","uuuu9999","88888888",new Endereco("Maceio","Tabuleirio","57084040","A 40","007","")));
-        }
-        UsuarioDao usuarioDao = new UsuarioDao();
-        List<Usuario> listUsuario = new ArrayList<Usuario>();
-        listUsuario = usuarioDao.listar();
+        usuario2 = new Usuario(
+                "Usuario Teste de Atualização",
+                "000",
+                "123456",
+                "email@hotmail.com",
+                "12341234",
+                "43214312",
+                false,
+                new Endereco(
+                        "Maceio",
+                        "B Bentes",
+                        "57084-040",
+                        "A 40",
+                        "44",
+                        ""
+                )
+        );        
         
-        for (Usuario usuario : listUsuario) {
-            System.out.println("Buca todos:> "+ usuario
-                    
-            );
-        }
-        usuarioDao.removerPorAtributo("nomeCompleto", "Tiago de Lima Alves");
+        usuarioDAO = new UsuarioDao();
+        super.setUp();
     }
-    
+
     @Test
-    public void deveEncontrarUmUsuarioPassandoIdComoParametro(){
-        try{
-            UsuarioDao usuarioDao = new UsuarioDao();
-            Usuario usuario = new Usuario();
-            usuario = usuarioDao.listarPorCpf("00066677700");
-        }catch(Exception ex){
-            System.out.println("Não foi possível localizar Usuario"+ex.getMessage());
+    public void testDeveriaInserirUsuarioNoBanco() throws Exception{
+        try {
+            usuarioDAO.salvar(usuario1);
+        } catch (Exception e) {
+
+            assertFalse(e.getMessage(), true);
+
+            throw new Exception(e.getMessage());
+        } finally {
+            collection.deleteOne(eq("cpf", "000"));
         }
     }
 
-    @Test//Refatorar futuramente 
-    public void deveAtualizarUmUsuarioPassandoIdNomeEmailComoParametro(){
-        try{
-            UsuarioDao usuarioDao = new UsuarioDao();
-            usuarioDao.atualizarPorId("5a255b126394faaaa39009a4", "emailteste@hotail", "TEste de atualização");
-        }catch(Exception ex){
-            System.out.println("Não foi possível atualizar usuario"+ex.getMessage());
-        }        
-    }
-    
-    @Test//Refatorar futuramente 
-    public void deveAtualizarUmUsuarioPassandoIdEObjeto(){
-        try{
-            Usuario usuario = new Usuario("Tiago de Lima Alves","000","123455","hotmail.com","uuuu9999","88888888",new Endereco("Maceio","Tabuleirio","57084040","A 40","007",""));
-            UsuarioDao usuarioDao = new UsuarioDao();
-            usuarioDao.atualizarPorId("5a2e73816394faaaa3cc1502", "hotmail.com", "Tiago de Lima Alves");
-        }catch(Exception ex){
-            System.out.println("Não foi possível atualizar usuario"+ex.getMessage());
-        }        
-    }
-    
     @Test
-    public void deveRemoverUmUsuarioPassandoIdComoParametro(){
-        try{
-            UsuarioDao usuarioDao = new UsuarioDao();
-            usuarioDao.removerPorCpf("00099988866");
-        }catch(Exception ex){
-            System.out.println("Não foi possível remover usuario"+ex.getMessage());
-        }                
-    }
-    
-    @Test
-    public void deveRemoverVariosUsuariosQueTenhamAChaveEOValorPassandoComoParametro(){
-        try{
-            UsuarioDao usuarioDao = new UsuarioDao();
-            usuarioDao.removerPorAtributo("nomeCompleto", "Tiago de Lima Alves");
-        }catch(Exception ex){
-            System.out.println("Não foi possível remover usuario"+ex.getMessage());
+    public void testDeveriaAlterarUsuarioCadastrado() throws Exception {
+        
+        try {
+            collection.insertOne(usuario1);
+            usuarioDAO.atualizarPorCpf("000", usuario2);
+        } catch (Exception e) {
+            assertFalse(e.getMessage(), true);
+            throw new Exception(e.getMessage());
+        } finally {
+            collection.deleteOne(eq("cpf", "000"));
         }
-    }*/
+    }
+    
+    @Test
+    public void testDeveriaExcluirUsuarioCadastradoValido() throws Exception {
+        try {
+            collection.insertOne(usuario1);
+            usuarioDAO.removerPorCpf("000");
+        } catch (Exception e) {
+            assertFalse(e.getMessage(), true);
+            throw new Exception(e.getMessage());
+        } finally {
+            //rollback();
+        }
+    }
+
+    
+    @Test
+    public void testDeveriaRetornarOsDadosDosUsuariosCadastrados() throws Exception {
+        try {
+            List<Usuario> listaUsuarios;
+
+            listaUsuarios = usuarioDAO.listar();
+
+            for (Usuario usuario : listaUsuarios) {
+                System.out.println(usuario.getNomeCompleto()+
+                        "\n"+usuario.getCpf()+
+                        "\n"+usuario.getEmail()+
+                        "\n"+usuario.getFone1()+
+                        "\n");
+            }
+
+        } catch (Exception e) {
+            assertFalse(e.getMessage(), true);
+        } finally {
+            //rollback();
+        }
+    }
+    
+    @Test
+    public void testDeveriaRetornarOsDadosDoUsuarioPeloCpf() throws Exception {
+        try {
+            collection.insertOne(usuario1);
+            Usuario usuario = usuarioDAO.listarPorCpf("000");
+
+            System.out.println(usuario.getNomeCompleto()
+                    + "\n" + usuario.getCpf()
+                    + "\n" + usuario.getEmail()
+                    + "\n" + usuario.getFone1()
+                    + "\n");
+
+        } catch (Exception e) {
+            assertFalse(e.getMessage(), true);
+        } finally {
+            collection.deleteOne(eq("cpf", "000"));
+        }
+    }
+    
+    @Test
+    public void testDeveriaRemoverUsuarioPeloCpf() throws Exception {
+        try {
+            collection.insertOne(usuario1);
+            usuarioDAO.removerPorCpf("000");
+
+        } catch (Exception e) {
+            assertFalse(e.getMessage(), true);
+        } finally {
+            collection.deleteOne(eq("cpf", "000"));
+        }
+    }    
 }
